4756474f12c5f689693fda455c1abf227c4f888f
Robert Basamac <rbasamac@gmail.com>
2023-11-21 21:41:55 +0200

    Initial Commit

---
+++ ShoeHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoeHealth
+//
+//  Created by Robert Basamac on 21.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoeHealth/ShoeHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeHealthApp.swift
+//  ShoeHealth
+//
+//  Created by Robert Basamac on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoeHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
