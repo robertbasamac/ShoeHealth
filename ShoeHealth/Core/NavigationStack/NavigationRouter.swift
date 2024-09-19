0c331de524456edb4cf40623a2847b26497dae1d
Robert Basamac <rbasamac@gmail.com>
2024-09-19 12:26:39 +0300

    Add NavigationRouter class description

--- ShoeHealth/Core/NavigationStack/NavigationRouter.swift
+++ ShoeHealth/Core/NavigationStack/NavigationRouter.swift
@@ -8,6 +8,17 @@
 import SwiftUI
 import HealthKit
 
+/// A `NavigationRouter` that manages the navigation state of the app's tabbed and sheet-based UI components.
+///
+/// The `NavigationRouter` is an `ObservableObject` that holds the state for the selected tab and various sheet
+/// presentations within the app. It allows for dynamic navigation control, enabling the app to handle multiple
+/// navigation scenarios such as showing detailed views or presenting sheets with specific content.
+///
+/// This class exposes the following properties:
+/// - `selectedTab`: The currently selected tab in the tab-based navigation.
+/// - `showSheet`: An optional `SheetType` enum instance representing the sheet that should be displayed.
+/// - `showShoeDetails`: An optional `Shoe` model representing the shoe details to be shown.
+///
 final class NavigationRouter: ObservableObject {
     
     @Published var selectedTab: TabItem = .shoes
@@ -18,12 +29,20 @@ final class NavigationRouter: ObservableObject {
 
 extension NavigationRouter {
     
+    /// The `SheetType` enum supports various sheet presentations:
+    /// - `.addShoe`: A sheet for adding a new shoe.
+    /// - `.setDefaultShoe`: A sheet for setting the default shoe.
+    /// - `.addWorkoutToShoe(workoutID: UUID)`: A sheet for adding a specific workout to a shoe.
+    /// - `.addMultipleWorkoutsToShoe(workoutIDs: [UUID])`: A sheet for adding multiple workouts to a shoe.
+    ///
+    /// The `SheetType` enum conforms to `Identifiable` by providing a unique `id` for each case, which is used to
+    /// identify the sheet currently being presented. Additionally, it conforms to `Equatable` to facilitate comparison
+    /// of different sheet types, enabling smooth transitions and updates to the UI.
     enum SheetType: Identifiable {
         case addShoe
         case setDefaultShoe
         case addWorkoutToShoe(workoutID: UUID)
         case addMultipleWorkoutsToShoe(workoutIDs: [UUID])
-
         
         var id: UUID {
             switch self {
