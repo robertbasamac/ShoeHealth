38797b2ec1370f248b3512fdbf5e0bec0b706943
Robert Basamac <rbasamac@gmail.com>
2024-11-11 22:07:52 +0200

    Refactor Privacy Policy and Terms of Service link location; Open In-App URLs using SFSafariViewController

--- ShoeHealth/Core/Store/Views/PaywallView.swift
+++ ShoeHealth/Core/Store/Views/PaywallView.swift
@@ -9,97 +9,30 @@ import SwiftUI
 import StoreKit
 
 struct PaywallView: View {
+    
     @EnvironmentObject private var navigationRouter: NavigationRouter
     @EnvironmentObject private var store: StoreManager
     
+    @Environment(\.openURL) private var openURL
     @Environment(\.dismiss) private var dismiss
     
     @State private var showManagedSubscriptions: Bool = false
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 12) {
-                if let product = store.lifetimeProduct {
-                    VStack(spacing: 12) {
-                        ProductView(product) { phase in
-                            switch phase {
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(.rect(cornerRadius: 25))
-                            case .failure(_):
-                                Image("ShoeHealth-unlimited")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(.rect(cornerRadius: 25))
-                            case .unavailable:
-                                Image("ShoeHealth-unlimited")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(.rect(cornerRadius: 25))
-                            case .loading:
-                                ProgressView()
-                            @unknown default:
-                                fatalError()
-                            }
-                        }
-                        .productViewStyle(.large)
-                        
-                        if store.hasFullAccess {
-                            if store.isPurchased(product) {
-                                VStack(spacing: 12) {
-                                    Text("You have lifetime access to all app features.")
-                                    
-                                    VStack(spacing: 4) {
-                                        Text("If you have any active subscriptions, consider canceling them as they are no longer necessary.")
-                                            .foregroundStyle(.white)
-                                        
-                                        Button {
-                                            showManagedSubscriptions.toggle()
-                                        } label: {
-                                            Text("Manage subscriptions")
-                                                .foregroundStyle(Color.theme.accent)
-                                        }
-                                    }
-                                }
-                                .font(.caption)
-                                .foregroundStyle(Color.theme.accent)
-                                .multilineTextAlignment(.center)
-                                .manageSubscriptionsSheet(isPresented: $showManagedSubscriptions)
-                            } else if let expirationDate = store.expirationDate {
-                                VStack(spacing: 12) {
-                                    Text("Subscribed until \(dateFormatter.string(from: expirationDate)).")
-                                    
-                                    Button {
-                                        showManagedSubscriptions.toggle()
-                                    } label: {
-                                        Text("Manage subscriptions")
-                                            .foregroundStyle(Color.theme.accent)
-                                    }
-                                }
-                                .font(.caption)
-                                .foregroundStyle(Color.theme.accent)
-                                .multilineTextAlignment(.center)
-                                .manageSubscriptionsSheet(isPresented: $showManagedSubscriptions)
-                            }
-                        }
-                    }
-                    .padding(.horizontal, 40)
+            if let product = store.lifetimeProduct {
+                VStack(spacing: 12) {
+                    lifetimeProductSection(product)
                     
-                    SubscriptionStoreView(subscriptions: store.subscriptionProducts) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            featureItem(title: "Unlimited shoes")
-                        }
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .padding(.horizontal, 40)
-                    }
-                    .disabled(store.isPurchased(product))
-                    .storeButton(.hidden, for: .cancellation)
-                    .subscriptionStoreControlStyle(.prominentPicker)
-                } else {
-                    SubscriptionStoreView(subscriptions: store.subscriptionProducts)
+                    subscriptionsSection
+                        .disabled(store.isPurchased(product))
+                    
+                    restorePurchasesButton
+                    
+                    termsOfUseAndPrivacyPolicyButtons
                 }
+            } else {
+                subscriptionsSection
             }
         }
         .scrollBounceBehavior(.basedOnSize)
@@ -121,6 +54,117 @@ struct PaywallView: View {
 // MARK: - View Components
 
 extension PaywallView {
+    
+    @ViewBuilder
+    private func lifetimeProductSection(_ product: Product) -> some View {
+        VStack(spacing: 12) {
+            ProductView(product) { phase in
+                switch phase {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(.rect(cornerRadius: 25))
+                case .failure(_):
+                    Image("ShoeHealth-unlimited")
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(.rect(cornerRadius: 25))
+                case .unavailable:
+                    Image("ShoeHealth-unlimited")
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(.rect(cornerRadius: 25))
+                case .loading:
+                    ProgressView()
+                @unknown default:
+                    fatalError()
+                }
+            }
+            .productViewStyle(.large)
+            
+            if store.hasFullAccess {
+                if store.isPurchased(product) {
+                    VStack(spacing: 12) {
+                        Text("You have lifetime access to all app features.")
+                        
+                        Text("If you have any active subscriptions, consider canceling them as they are no longer necessary.")
+                            .foregroundStyle(.white)
+                        
+                        Button {
+                            showManagedSubscriptions.toggle()
+                        } label: {
+                            Text("Manage subscriptions")
+                                .foregroundStyle(Color.theme.accent)
+                        }
+                    }
+                    .font(.caption)
+                    .foregroundStyle(Color.theme.accent)
+                    .multilineTextAlignment(.center)
+                    .manageSubscriptionsSheet(isPresented: $showManagedSubscriptions)
+                } else if let expirationDate = store.expirationDate {
+                    VStack(spacing: 12) {
+                        Text("Subscribed until \(dateFormatter.string(from: expirationDate)).")
+                        
+                        Button {
+                            showManagedSubscriptions.toggle()
+                        } label: {
+                            Text("Manage subscriptions")
+                                .foregroundStyle(Color.theme.accent)
+                        }
+                    }
+                    .font(.caption)
+                    .foregroundStyle(Color.theme.accent)
+                    .multilineTextAlignment(.center)
+                    .manageSubscriptionsSheet(isPresented: $showManagedSubscriptions)
+                }
+            }
+        }
+        .padding(.horizontal, 40)
+    }
+    
+    @ViewBuilder
+    private var subscriptionsSection: some View {
+        SubscriptionStoreView(subscriptions: store.subscriptionProducts) {
+            featuresSection
+        }
+        .subscriptionStoreControlStyle(.prominentPicker)
+//        .storeButton(.hidden, for: .cancellation)
+    }
+    
+    @ViewBuilder
+    private var featuresSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            featureItem(title: "Unlimited Shoes")
+        }
+        .frame(maxWidth: .infinity, alignment: .center)
+        .padding(.horizontal, 40)
+    }
+    
+    @ViewBuilder
+    private var restorePurchasesButton: some View {
+        Button {
+            Task {
+                try? await AppStore.sync()
+            }
+        } label: {
+            Text("Restore Purchases")
+                .fontWeight(.semibold)
+        }
+    }
+    
+    @ViewBuilder
+    private var termsOfUseAndPrivacyPolicyButtons: some View {
+        HStack(spacing: 4) {
+            Link("Terms of Service", destination: URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/")!)
+            
+            Text("and")
+            
+            Link("Privacy Policy", destination: URL(string: "https://github.com/robertbasamac/ShoeHealth/blob/master/APP_POLICY.md")!)
+        }
+        .font(.footnote)
+        .handleOpenURLInApp()
+    }
     
     @ViewBuilder
     private func featureItem(title: String) -> some View {
---
+++ ShoeHealth/Helpers/SFSafariViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SFSafariViewController.swift
+//  ShoeHealth
+//
+//  Created by Robert Basamac on 11.11.2024.
+//
+
+import UIKit
+import SwiftUI
+import SafariServices
+
+// MARK: - UIViewControllerRepresentable
+struct SFSafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<Self>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SFSafariView>) {
+        // No need to do anything here
+    }
+}
+
+// MARK: - View Modifier
+private struct SafariViewControllerViewModifier: ViewModifier {
+    
+    @State private var urlToOpen: URL?
+
+    func body(content: Content) -> some View {
+        content
+            .environment(\.openURL, OpenURLAction { url in
+                /// Catch any URLs that are about to be opened in an external browser.
+                /// Instead, handle them here and store the URL to reopen in our sheet.
+                urlToOpen = url
+                return .handled
+            })
+            .sheet(isPresented: $urlToOpen.mappedToBool(), onDismiss: {
+                urlToOpen = nil
+            }, content: {
+                SFSafariView(url: urlToOpen!)
+            })
+    }
+}
+
+extension View {
+    
+    /// Monitor the `openURL` environment variable and handle them in-app instead of via
+    /// the external web browser.
+    /// Uses the `SafariViewWrapper` which will present the URL in a `SFSafariViewController`.
+    func handleOpenURLInApp() -> some View {
+        modifier(SafariViewControllerViewModifier())
+    }
+}
+
+// MARK: - Binding extensions
+
+extension Binding where Value == Bool {
+    
+    init(binding: Binding<(some Any)?>) {
+        self.init(
+            get: {
+                binding.wrappedValue != nil
+            },
+            set: { newValue in
+                guard newValue == false else { return }
+
+                // We only handle `false` booleans to set our optional to `nil`
+                // as we can't handle `true` for restoring the previous value.
+                binding.wrappedValue = nil
+            }
+        )
+    }
+}
+
+extension Binding {
+    
+    /// Maps an optional binding to a `Binding<Bool>`.
+    /// This can be used to, for example, use an `Error?` object to decide whether or not to show an
+    /// alert, without needing to rely on a separately handled `Binding<Bool>`.
+    func mappedToBool<Wrapped>() -> Binding<Bool> where Value == Wrapped? {
+        Binding<Bool>(binding: self)
+    }
+}
